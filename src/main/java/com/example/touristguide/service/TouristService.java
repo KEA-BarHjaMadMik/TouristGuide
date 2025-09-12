6a756184bbc91465d60dc44dc529dbba42c37100
Mbroholm <mibr0005@stud.kea.dk>
2025-09-12 10:08:32 +0200

    [CodeWithMe] removed unused import statements

--- src/main/java/com/example/touristguide/service/TouristService.java
+++ src/main/java/com/example/touristguide/service/TouristService.java
@@ -6,7 +6,6 @@ import com.example.touristguide.model.TouristAttraction;
 import com.example.touristguide.repository.TouristRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
