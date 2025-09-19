8ac76a38575860d0c3f92f865a712797cf856276
Mbroholm <mibr0005@stud.ek.dk>
2025-09-20 01:33:39 +0200

    Added price registration functionality and currency conversion

--- src/main/java/com/example/touristguide/service/TouristService.java
+++ src/main/java/com/example/touristguide/service/TouristService.java
@@ -1,11 +1,14 @@
 package com.example.touristguide.service;
 
 import com.example.touristguide.model.AttractionTags;
+import com.example.touristguide.model.CurrencyRates;
 import com.example.touristguide.model.DanishCity;
 import com.example.touristguide.model.TouristAttraction;
 import com.example.touristguide.repository.TouristRepository;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +21,37 @@ public class TouristService {
 
     public List<TouristAttraction> getAttractions() {
         return repository.getAttractions();
+    }
+
+    public List<TouristAttraction> getAttractionsInCurrency(String currencyCode) {
+        List<TouristAttraction> attractions = repository.getAttractions();
+
+        // get rates from repository
+        double conversionRate = 1.0;
+        try {
+            CurrencyRates currencyRates = repository.getCurrencyRates();
+            switch (currencyCode.toUpperCase()) {
+                case "EUR" -> conversionRate = currencyRates.getEUR() / currencyRates.getDKK();
+                case "USD" -> conversionRate = currencyRates.getUSD() / currencyRates.getDKK();
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to get currency rates: " + e.getMessage());
+        }
+
+        // generate new converted attraction list
+        List<TouristAttraction> convertedAttractions = new ArrayList<>();
+        for(TouristAttraction attraction : attractions) {
+            TouristAttraction convertedAttraction = new TouristAttraction(
+                    attraction.getName(),
+                    attraction.getDescription(),
+                    attraction.getCity(),
+                    attraction.getTags(),
+                    attraction.getTicketPrice() * conversionRate
+            );
+            convertedAttractions.add(convertedAttraction);
+        }
+
+        return convertedAttractions;
     }
 
     public TouristAttraction findAttractionByName(String name) {
@@ -36,11 +70,11 @@ public class TouristService {
         return repository.deleteAttraction(name);
     }
 
-    public List<DanishCity> getCities(){
+    public List<DanishCity> getCities() {
         return repository.getCities();
     }
 
-    public List<AttractionTags> getTags(){
+    public List<AttractionTags> getTags() {
         return repository.getTags();
     }
 }
